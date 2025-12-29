3275a8da23c40b6a8f9f0cb5a26c52c382330312
Hanz Bacus <bhanzchester@gmail.com>
2025-12-29 08:02:48 +0800

    django-supabase setup

---
+++ utils.py
@@ -0,0 +1,8 @@
+# In a utils.py or config file
+from supabase import create_client, Client
+from django.conf import settings
+
+supabase: Client = create_client(
+    settings.SUPABASE_URL,
+    settings.SUPABASE_KEY
+)
